7890d3b8011c70e67ee69cb02c1f521d3f4a3f20
mgnyba1 <mgnyba1@gmail.com>
2021-06-25 12:31:37 -0400

    Added DD mode and some features...

--- main.py
+++ main.py
@@ -1,38 +1,88 @@
 
 import os
 import sys
+from multiprocessing import Process
 from os import listdir
 from blkinfo import BlkDiskInfo
 from pySMART import Device
 import subprocess
 from pip._internal import exceptions
 from subprocess import check_output
+import asyncio
+from concurrent.futures import ProcessPoolExecutor
+
+DD_MODE = False;
+CHECK_SMART = True;
+
+async def wipe_disk(disk_name):
+
+
+
+    print(f"Rozpoczynam czyszczenie dysku: {disk_name}");
+
+    if CHECK_SMART:
+        proc1 = await asyncio.create_subprocess_exec("./smartctl", "-a", f"/dev/{disk_name}",stdout=asyncio.subprocess.PIPE,stderr=asyncio.subprocess.PIPE)
+        stdout2, stderr2 = await proc1.communicate()
+
+        f = open(f"Smarty/SMART_PRZED_{disk_name}.txt", "w")
+        f.write(f"\r\n\r\nSMART DYSKU {disk_name}: {stdout2}")
+
+
+
+    if DD_MODE:
+        proc = await asyncio.create_subprocess_exec("./dd", f"if=/dev/zero", f"of=/dev/{disk_name}", f"bs=64k",stdout=asyncio.subprocess.PIPE,stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await proc.communicate()
+        print(stdout)
+
+    else:
+        proc = await asyncio.create_subprocess_exec("./badblocks", "-sv", f"/dev/{disk_name}",stdout=asyncio.subprocess.PIPE,stderr=asyncio.subprocess.PIPE)
+        stdout, stderr = await proc.communicate()
+        print(stdout)
+
+
+
+    if CHECK_SMART:
+        proc3 = await asyncio.create_subprocess_exec("./smartctl", "-a", f"/dev/{disk_name}",stdout=asyncio.subprocess.PIPE,stderr=asyncio.subprocess.PIPE)
+        stdout3, stderr3 = await proc3.communicate()
+
+        f = open(f"Smarty/SMART_PO_{disk_name}.txt", "w")
+        f.write(f"\r\n\r\nSMART DYSKU {disk_name}: {stdout3}")
+
+
 
 if __name__ == '__main__':
     myblkd = BlkDiskInfo()
     all_my_disks = myblkd.get_disks()
 
-    print("Wybierz dysk do czyszczenia (wpisz nr i zatwierdz):")
+    print("Wybierz tryb: BADBLOCKS (Wpisz: BB), DD (Wpisz: DD) i zatwierdz:")
 
-    i = 1;
-    for disk in all_my_disks:
-        print(f"{i}. {disk['name']}")
-        i = i + 1;
+    tryb = input()
 
-    choosen_number = int(input())
+    if tryb == "BB":
+        DD_MODE = False;
+    else:
+        DD_MODE = True;
 
-    disk_to_shred = all_my_disks[choosen_number-1]
+    print("Wybierz sprawdzanie SMART: (0) WYLACZONY (1) WLACZONY")
+
+    smart = input()
+
+    if(smart == "0"):
+        CHECK_SMART = False;
+    else:
+        CHECK_SMART = True;
+
+    print("Wpisz nazwe dysku systemowego np. sda. Ten dysk nie bedzie czyszczony.")
+    nazwa_dysku_systemowego = input()
 
 
-    print(f"Wybrany dysk: {disk_to_shred['name']}.")
+    loop = asyncio.get_event_loop()
 
+    tasks = [
+        loop.create_task(wipe_disk("sdb")),
+        loop.create_task(wipe_disk("sdd")),
+    ]
 
-    out = check_output(["./smartctl", "-a", f"/dev/{disk_to_shred['name']}"])
-    f = open("SMART.txt", "a")
-    f.write(f"\r\n\r\nSMART DYSKU {disk_to_shred['name']}: {out}")
-    print("SMART Zapisany do pliku SMART.txt");
-
-    print("Rozpoczynam czyszczenie...");
-
-    out = check_output(["./badblocks", "-sv", f"/dev/{disk_to_shred['name']}"])
-    print(out)
+    loop.set_default_executor(ProcessPoolExecutor())
+    loop.run_until_complete(asyncio.wait(tasks))
+    loop.close()
